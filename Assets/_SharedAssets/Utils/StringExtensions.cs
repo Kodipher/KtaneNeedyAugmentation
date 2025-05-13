f2a578faff31809202a4496a8a92306d0c53705b
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-05-13 21:12:34 +0200

    Add StringExtensions

---
+++ Assets/_SharedAssets/Utils/StringExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace SharedAssets.Utils {
+
+	public static class StringExtentions {
+
+		static readonly Regex regexContainsEnglishLetters = new Regex(@"[a-zA-Z]");
+
+		/// <summary>
+		/// Check if a sting contains letters (a through z)
+		/// </summary>
+		public static bool ContainsModernEnglishLetters(this string str) {
+			return regexContainsEnglishLetters.IsMatch(str);
+		}
+
+		internal static readonly Regex regexFindColorTags = new Regex(@"<(?<close>\/)?color(?(close)|=(?<color>[^>]*))?>", RegexOptions.IgnoreCase);
+
+		/// <summary>Removes color tags from the string</summary>
+		/// <returns>A string without color tags</returns>
+		public static string RemoveColorTags(this string str) {
+
+			StringBuilder stringBuilder = new StringBuilder(str.Length);
+			int copyStartI = 0;
+
+			// For all individual color tags
+			foreach (Match match in regexFindColorTags.Matches(str)) {
+				// Copy until tag
+				stringBuilder.Append(str, copyStartI, match.Index - copyStartI);
+				// Skip the tag itself
+				copyStartI = match.Index + match.Length;
+			}
+
+			// Copy reaminging chars
+			stringBuilder.Append(str, copyStartI, str.Length - copyStartI);
+
+			// Return
+			return stringBuilder.ToString();
+		}
+
+
+		/// <summary>No-Break space character</summary>
+		public const char nbsp = '\u00A0';
+
+		/// <summary>
+		/// Returns given string with underscores replaced by nbsp (no-break space)
+		/// </summary>
+		public static string UnderscoreToNoBreakSpace(this string str) {
+			return str.Replace('_', nbsp);
+		}
+
+		/// <summary>Returns the number of english vowels in the string.</summary>
+		public static int CountEnglishVowels(this string str, bool includeY = false) {
+
+			int count = 0;
+
+			for (int i = 0; i < str.Length; i++) {
+				char chr = char.ToUpperInvariant(str[i]);
+
+				if (chr < 'A' || chr > 'Z') continue;
+				if (chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U') count++;
+				if (includeY && chr == 'Y') count++; 
+			}
+
+			return count;
+		}
+
+	}
+
+}
