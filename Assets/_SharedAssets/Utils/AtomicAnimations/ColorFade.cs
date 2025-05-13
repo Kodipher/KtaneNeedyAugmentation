af1faee1e2b4258ee562ea8eb998c811c16a1109
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-05-13 21:11:45 +0200

    Add Color animation atoms

---
+++ Assets/_SharedAssets/Utils/AtomicAnimations/ColorFade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using Rephidock.AtomicAnimations;
+using Rephidock.AtomicAnimations.Base;
+
+
+namespace SharedAssets.Utils.AtomicAnimations {
+
+	public class ColorFade : Ease {
+
+		readonly Color oldColor;
+		readonly Color newColor;
+		readonly Action<Color> setter;
+
+		public ColorFade(
+			Color oldColor,
+			Color newColor,
+			TimeSpan duration,
+			EasingCurve easing,
+			Action<Color> setter
+		) : base(duration, easing) {
+			this.oldColor = oldColor;
+			this.newColor = newColor;
+			this.setter = setter;
+		}
+
+		protected override void EaseUpdateImpl(float valueProgressNew) {
+			Color lerpedColor = Color.Lerp(oldColor, newColor, valueProgressNew);
+			setter(lerpedColor);
+		}
+
+	}
+
+}
---
+++ Assets/_SharedAssets/Utils/AtomicAnimations/DualColorFade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using Rephidock.AtomicAnimations;
+using Rephidock.AtomicAnimations.Base;
+
+
+namespace SharedAssets.Utils.AtomicAnimations {
+
+	public class DualColorFade : Ease {
+
+		readonly Color oldColor1;
+		readonly Color oldColor2;
+		readonly Color newColor1;
+		readonly Color newColor2;
+		readonly Action<Color, Color> setter;
+
+		public DualColorFade(
+			Color oldColor1,
+			Color oldColor2,
+			Color newColor1,
+			Color newColor2,
+			TimeSpan duration,
+			EasingCurve easing,
+			Action<Color, Color> setter
+		) : base(duration, easing) {
+			this.oldColor1 = oldColor1;
+			this.oldColor2 = oldColor2;
+			this.newColor1 = newColor1;
+			this.newColor2 = newColor2;
+			this.setter = setter;
+		}
+
+		protected override void EaseUpdateImpl(float valueProgressNew) {
+			Color lerpedColor1 = Color.Lerp(oldColor1, newColor1, valueProgressNew);
+			Color lerpedColor2 = Color.Lerp(oldColor2, newColor2, valueProgressNew);
+			setter(lerpedColor1, lerpedColor2);
+		}
+
+	}
+
+}
