37ebc5e49e7aa05ae9b1a172884c57350a42d76a
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-05-15 21:23:30 +0200

    Add TemplateModule boilerplate

--- Assets/_TemplateMod/TemplateModule.cs
+++ Assets/_TemplateMod/TemplateModule.cs
@@ -1,4 +1,14 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+using Rephidock.AtomicAnimations;
+using Rephidock.AtomicAnimations.Coroutines;
+
+using SharedAssets.Utils;
+
+using TimeSpan = System.TimeSpan;
+
 
 namespace TemplateMod {
 
@@ -6,6 +16,76 @@ namespace TemplateMod {
 	[RequireComponent(typeof(KMBombInfo))]
 	[RequireComponent(typeof(KMAudio))]
 	public class TemplateModule : MonoBehaviour {
+
+		#region //// State
+
+		private enum State {
+		}
+
+		State state;
+
+		#endregion
+
+		#region //// Components and parts
+
+		// Created 
+		internal KMBombModule kmModule;
+		internal KMBombInfo kmBomb;
+		internal KMAudio kmAudio;
+
+		internal System.Random rng;
+		internal ModuleLogger logger;
+		internal AnimationRunner animationRunner;
+		internal TemplateSettings settings;
+
+		private void PrepareComponents() {
+
+			// KM
+			kmModule = GetComponent<KMBombModule>();
+			kmBomb = GetComponent<KMBombInfo>();
+			kmAudio = GetComponent<KMAudio>();
+
+			kmModule.OnActivate += OnActivate;
+
+			// Misc. common
+			logger = new ModuleLogger(kmModule);
+			rng = new System.Random(UnityEngine.Random.Range(0, int.MaxValue));
+			animationRunner = new AnimationRunner();
+			settings = SettingsReader<TemplateSettings>.ReadSettings();
+
+		}
+
+		#endregion
+
+		#region //// Events
+
+		void Start() {
+
+			// Init
+			PrepareComponents();
+		}
+
+		void OnActivate() {
+
+		}
+
+		void Update() {
+			animationRunner?.Update(TimeSpan.FromSeconds(Time.deltaTime));
+		}
+
+		void OnDestroy() {
+			animationRunner?.Dispose();
+		}
+
+		#endregion
+
+		#region //// Routines
+
+		IEnumerable<CoroutineYield> TemplateRoutine() {
+			yield break;
+		}
+
+		#endregion
 
 	}
 
