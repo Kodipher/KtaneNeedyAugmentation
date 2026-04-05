1bd2c2e68ab10bb2cd6958b91d0681471f499d13
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-04-05 23:40:44 +0200

    Create MonoColoredString.cs

---
+++ Assets/_SharedAssets/Utils/Collections/MonoColoredString.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Color = UnityEngine.Color;
+using ColorUtility = UnityEngine.ColorUtility;
+
+
+namespace SharedAssets.Utils.Collections {
+
+	/// <summary>
+	/// <para>
+	/// Represents a string with an (optional) <see cref="Color"/>.
+	/// Unlike <see cref="ColoredString"/>, only stores 1 color for all characters.
+	/// </para>
+	/// <para>
+	/// Immutable.
+	/// </para>
+	/// </summary>
+	public /* record */ class MonoColoredString {
+
+		#region /--- Storage and Creation ---/
+		
+		public string String { get; /* init; */ }
+		
+		public Color? Color { get; /* init; */ }
+
+		public MonoColoredString() {
+			String = "";
+			Color = null;
+		}
+		
+		public MonoColoredString(string str, Color? col = null) {
+
+			if (str == null) throw new ArgumentNullException(nameof(str));
+			
+			String = str;
+			Color = col;
+		}
+
+		public MonoColoredString WithColor(Color? col) => new MonoColoredString(this.String, col);
+		
+		public MonoColoredString WithString(string str) => new MonoColoredString(str, this.Color);
+		
+		#endregion
+
+		#region /--- Conversions ---/
+		
+		public override string ToString() => this.String;
+		
+		/// <summary>
+		/// Wraps the <see cref="String"/> in the color tag
+		/// with <see cref="Color"/> as the color, is not null.
+		/// </summary>
+		public string ToRichString() {
+			
+			if (!this.Color.HasValue) return this.String;
+
+			string colorString = ColorUtility.ToHtmlStringRGBA(this.Color.Value);
+			return $"<color=#{colorString}>{this.String}</color>";
+		}
+		
+		public ColoredString ToColoredString() => new ColoredString(this.String, this.Color);
+		
+		#endregion
+		
+	}
+
+}
