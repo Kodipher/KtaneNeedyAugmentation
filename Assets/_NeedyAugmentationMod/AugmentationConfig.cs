ad0ed41f9d8d4bdb701f397ef62c3ae9b4e1db41
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-07-15 19:35:16 +0200

    Add property assigning logic

--- Assets/_NeedyAugmentationMod/AugmentationConfig.cs
+++ Assets/_NeedyAugmentationMod/AugmentationConfig.cs
@@ -17,6 +17,47 @@ namespace NeedyAugmentationMod {
 		public ReadOnlyCollection<AugmentationPropertySet> PropertySets { get; private set; } =
 										new ReadOnlyCollection<AugmentationPropertySet>(new AugmentationPropertySet[0]);
 		
+		readonly Dictionary<string, List<AugmentationPropertySet>> propertiesById = new Dictionary<string, List<AugmentationPropertySet>>();
+		readonly Dictionary<string, int> nextIndexToAssignById = new Dictionary<string, int>();
+		
+		void ConfirmProperties(IList<AugmentationPropertySet> propertySets) {
+			
+			PropertySets = new ReadOnlyCollection<AugmentationPropertySet>(propertySets);
+
+			foreach (var propertySet in propertySets) {
+				
+				// Dupe id
+				List<AugmentationPropertySet> dupe;
+				if (propertiesById.TryGetValue(propertySet.ModuleId, out dupe)) {
+					dupe.Add(propertySet);
+					continue;
+				}
+				
+				// New id
+				propertiesById.Add(propertySet.ModuleId, new List<AugmentationPropertySet>());
+				nextIndexToAssignById.Add(propertySet.ModuleId, 0);
+				
+			}
+		}
+		
+		[CanBeNull] public AugmentationPropertySet AssignPropertySet(string moduleId) {
+			
+			if (moduleId == "whiteModule") return null; // Black and White are linked; Black has authority.
+			
+			List<AugmentationPropertySet> dupes;
+			if (propertiesById.TryGetValue(moduleId, out dupes)) {
+				int index = nextIndexToAssignById[moduleId];
+				nextIndexToAssignById[moduleId] = (index + 1) % dupes.Count;
+				return dupes[index];
+			}
+			
+			if (moduleId != WildcardId) {
+				return AssignPropertySet(WildcardId);
+			}
+			
+			return null;
+		}
+
 	}
 
 }
