d0e573222d89f1ab5637f18eeaf0f166e83a2911
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-07-15 19:34:39 +0200

    Create AugmentationConfig.cs

---
+++ Assets/_NeedyAugmentationMod/AugmentationConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using JetBrains.Annotations;
+
+
+namespace NeedyAugmentationMod {
+
+	public class AugmentationConfig {
+
+		public const string WildcardId = "*";
+
+		public ReadOnlyCollection<AugmentationPropertySet> PropertySets { get; private set; } =
+										new ReadOnlyCollection<AugmentationPropertySet>(new AugmentationPropertySet[0]);
+		
+	}
+
+}
