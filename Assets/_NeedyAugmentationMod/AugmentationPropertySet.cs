bf7092f93009385edb415c98b8b7265fe001c66d
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-07-15 19:34:04 +0200

    Create AugmentationPropertySet.cs

---
+++ Assets/_NeedyAugmentationMod/AugmentationPropertySet.cs
@@ -0,0 +1,27 @@
+﻿namespace NeedyAugmentationMod {
+
+	public /*record*/ class AugmentationPropertySet {
+
+		public /*required*/ string ModuleId { get; set; } = "";
+			
+		public float? InitialActivationTime { get; set; } = null;
+
+		public float? CooldownMultiplier { get; set; } = null;
+		public float? CooldownAddend { get; set; } = null;
+
+		public int? ActivationLimit { get; set; } = null;
+		public int? ActivationLimitAddendPerSolves { get; set; } = null;
+		public int? ActivationLimitSolves { get; set; } = null;
+
+		public float? StartThresholdSeconds { get; set; } = null;
+		public int? StartThresholdModules { get; set; } = null;
+			
+		public float? StopThresholdSeconds { get; set; } = null;
+		public int? StopThresholdModules { get; set; } = null;
+
+		public float? TerminationThresholdSeconds { get; set; } = null;
+		public int? TerminationThresholdModules { get; set; } = null;
+			
+	}
+
+}
