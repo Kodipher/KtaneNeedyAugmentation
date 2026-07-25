bdc00fc1107ec1b3d8dc8a7663852da1fde29755
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-07-25 12:22:56 +0200

    Add TurnOffPrefix patch

--- Assets/_NeedyAugmentationMod/IntegrationHelper.Patcher.cs
+++ Assets/_NeedyAugmentationMod/IntegrationHelper.Patcher.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
+using System.Reflection;
 using HarmonyLib;
+
+using System.Diagnostics.CodeAnalysis;
 
 
 namespace NeedyAugmentationMod {
 
 	public static partial class IntegrationHelper {
+		
+		[SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Haromy Injections")]
 		public static class Patcher {
 
 			public const string HarmonyId = "kodipher.NeedyAugmentation";
 			
-			static Harmony harmony = new Harmony(HarmonyId);
+			static readonly Harmony harmony = new Harmony(HarmonyId);
 			
 			/// <remarks>May throw.</remarks>
 			public static void EnsurePatched() {
@@ -17,10 +23,34 @@ namespace NeedyAugmentationMod {
 				Patch();
 			}
 			
+			
+			public const BindingFlags AllMethodFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+			
 			/// <remarks>May throw.</remarks>
 			static void Patch() {
-				throw new NotImplementedException();
+
+				MethodInfo original;
+				MethodInfo prefix;
+				
+				// -----
+				
+				// Turn off
+				// This method is called by this mod and when the bomb is solved or explodes
+				// patch: prevent emitting the deactivation event twice
+				original = needyComponentType.GetMethod("TurnOff", AllMethodFlags);
+				prefix = typeof(Patcher).GetMethod(nameof(TurnOffPrefix), AllMethodFlags);
+				harmony.Patch(original, prefix: new HarmonyMethod(prefix));
 			}
+
+			#region /--- Patches ---/
+			
+			static bool TurnOffPrefix(Component __instance) {
+				int currentState = __instance.GetValue<int>("State");
+				if (currentState == (int)NeedyComponentProxy.NeedyState.BombComplete) return false; // skip double trigger
+				return true;
+			}
+
+			#endregion
 			
 		}
 	}
