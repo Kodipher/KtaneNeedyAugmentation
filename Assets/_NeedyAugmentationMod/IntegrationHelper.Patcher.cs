9ae36d4d031f1d5090447c733a8614fc17e096f5
Kodipher <45706822+Kodipher@users.noreply.github.com>
2026-07-26 18:45:27 +0200

    Add ResetAndStartGuardInfix in StartRunning

--- Assets/_NeedyAugmentationMod/IntegrationHelper.Patcher.cs
+++ Assets/_NeedyAugmentationMod/IntegrationHelper.Patcher.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 
 using System.Diagnostics.CodeAnalysis;
@@ -32,16 +36,16 @@ namespace NeedyAugmentationMod {
 			}
 			
 			/*
-			 * Original flow:
+			 * Flow of logic, with `!!!` marking patches.
 			 * 
-			 * OnBombTimerTick -> [hasStarted guard] -> [elapsed guard] -> StartRunning;
-			 * ComponentStrikeEvent,ComponentPassEvent -> [state guard] -> PlayerChangedBomb
-			 * PlayerChangedBomb -> [coroutine] -> [state guard] -> [rng/dupe guard] -> StartRunning;
-			 * StartRunning -> [hasStarted guard] -> ResetAndStart;
-			 * WaitAndResetRoutine -> [coroutine] -> ChangeState(Cooldown) -> ResetAndStart
+			 * OnBombTimerTick -> !!! -> [hasStarted guard] -> [elapsed guard] -> StartRunning();
+			 * ComponentStrikeEvent,ComponentPassEvent -> [state guard] -> PlayerChangedBomb(tinyDelay);
+			 * PlayerChangedBomb -> !!! -> [coroutine] -> [state guard] -> [rng/dupe guard] -> StartRunning();
+			 * StartRunning -> [hasStarted guard] -> !!! ResetAndStart();
+			 * WaitAndResetRoutine -> [coroutine] -> ChangeState(Cooldown) -> ResetAndStart()
 			 * ResetAndStart -> ChangeState(Running)
-			 * OnSolved,OnTimerExpired -> [shouldReset check] -> [if true] WaitAndResetRoutine / [if false] ChangeState(Terminated)
-			 * TurnOff -> ChangeState(BombComplete)
+			 * OnSolved,OnTimerExpired -> [shouldReset check] -> [if true] WaitAndResetRoutine() / [if false] ChangeState(Terminated)
+			 * TurnOff -> !!! -> ChangeState(BombComplete)
 			 *
 			 */
 			
@@ -52,6 +56,7 @@ namespace NeedyAugmentationMod {
 
 				MethodInfo original;
 				MethodInfo prefix;
+				MethodInfo transpiler;
 				
 				// -----
 				
@@ -63,11 +68,18 @@ namespace NeedyAugmentationMod {
 				
 				// PlayerChangedBomb
 				// patch: if AugmentedActivatorComponent exists, add its state guard
-				// This patch is a bit dirty, but this way the patch is only called for correct instances
-				// without the use of a transpiler
+				// This patch is a bit dirty, but this way you do not need a transpiler
 				original = needyComponentType.GetMethod("PlayerChangedBomb", AllMethodFlags);
 				prefix = typeof(Patcher).GetMethod(nameof(PlayerChangedBombPrefix), AllMethodFlags);
 				harmony.Patch(original, prefix: new HarmonyMethod(prefix) { priority = Priority.VeryLow });
+				
+				// StartRunning
+				// patch: wrap ResetAndStart in ResetAndStartGuardInfix
+				// The infix checks if AugmentedActivatorComponent exists and, if it does,
+				// notifies it that a needy wants to activate (through a guard call) 
+				original = needyComponentType.GetMethod("StartRunning", AllMethodFlags);
+				transpiler = typeof(Patcher).GetMethod(nameof(GuardResetAndStartTranspiler), AllMethodFlags);
+				harmony.Patch(original, transpiler: new HarmonyMethod(transpiler));
 				
 				
 				// TurnOff
@@ -118,6 +130,96 @@ namespace NeedyAugmentationMod {
 				
 				// Interrupting...
 				return true;
+			}
+			
+			static IEnumerable<CodeInstruction> GuardResetAndStartTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator) {
+				
+				bool patched = false;
+				
+				MethodInfo resetAndStartMethod = needyComponentType.GetMethod("ResetAndStart", AllMethodFlags);
+				
+				List<CodeInstruction> instructionList = instructions.ToList();
+				for (int i = 0; i < instructionList.Count; i++) {
+					
+					// Unless `this.ResetAndStart()` ...
+					if (  
+					    !(
+						    instructionList[i].IsLdarg(0) &&
+							i + 1 < instructionList.Count &&
+							instructionList[i + 1].Is(OpCodes.Callvirt, resetAndStartMethod)
+					    )
+					) {
+						// ... leave as is
+						yield return instructionList[i];
+						continue;
+					}
+
+					// Wrap `this.ResetAndStart()` in a ResetAndStartGuardInfix check and notifier
+					//
+					// ```
+					// .maxstack 2
+					//	.locals init (
+					//	[0] bool V_0
+					// )
+					//
+					// IL_0000: nop
+					// IL_0001: ldarg.0      // this
+					// IL_0002: ldloca.s     V_0
+					// IL_0004: call         void NeedyAugmentationMod.IntegrationHelper/Patcher::Infix(object, bool&)
+					// IL_0009: ldloc.0      // V_0
+					// IL_000a: brfalse      IL_0017
+					// IL_000f: nop
+					// IL_0010: ldarg.0      // this
+					// IL_0011: call         instance void NeedyComponent::ResetAndStart()
+					// IL_0016: nop
+					// IL_0017: nop
+					// ``
+
+					// bool isActivating;
+					var isActivatingLocal = ilGenerator.DeclareLocal(typeof(bool));
+						
+					// ResetAndStartGuardInfix(this, ref isActivating);
+					yield return new CodeInstruction(OpCodes.Ldarg_0);
+					yield return new CodeInstruction(OpCodes.Ldloca_S, isActivatingLocal.LocalIndex);
+					yield return CodeInstruction.Call(typeof(Patcher), nameof(ResetAndStartGuardInfix));
+						
+					// if (!isActivatingLocal)
+					var skipCallLabel = ilGenerator.DefineLabel();
+					yield return new CodeInstruction(OpCodes.Ldloc_0);
+					yield return new CodeInstruction(OpCodes.Brfalse, skipCallLabel);
+					
+					// {
+					// this.ResetAndStart();
+					yield return instructionList[i];
+					yield return instructionList[i + 1];
+						
+					// }
+					yield return new CodeInstruction(OpCodes.Nop).WithLabels(skipCallLabel);
+
+					patched = true;
+
+					i++; // consume ldarg.0
+					continue; // consume call and continue
+				}
+
+				if (!patched) throw new ArgumentException("Could not find the original method call to wrap.");
+			}
+			
+			static void ResetAndStartGuardInfix(Component __instance, out bool isActivating) {
+				
+				AugmentedActivatorComponent activator;
+				if (!AugmentedActivatorComponent.TryGetCached(__instance.GetInstanceID(), out activator)) {
+					// No component
+					isActivating = true;
+					return;
+				}
+
+				if (activator.IsInStoppedState()) {
+					isActivating = false;
+					return;
+				}
+				
+				activator.GuardActivationBySolves(out isActivating);
 			}
 
 			#endregion
